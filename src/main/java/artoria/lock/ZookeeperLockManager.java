51330147dc86d68e1c9fdf5d81f0557b084af8ae
Kahle <kahle@undefined>
2022-03-31 22:09:38 +0800

    Optimize lock tools related code

---
+++ src/main/java/artoria/lock/ZookeeperLockManager.java
@@ -0,0 +1,129 @@
+package artoria.lock;
+
+import artoria.common.Constants;
+import artoria.exception.ExceptionUtils;
+import artoria.util.Assert;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+import static artoria.common.Constants.SLASH;
+
+/**
+ * The zookeeper reentrant lock manager.
+ * @see org.apache.curator.framework.recipes.locks.InterProcessMutex
+ * @author Kahle
+ */
+public class ZookeeperLockManager implements LockManager {
+    private static final Logger log = LoggerFactory.getLogger(ZookeeperLockManager.class);
+    private static final String PATH_PREFIX = "/lock/reentrant";
+    private final Map<String, InterProcessMutex> storage;
+    private final CuratorFramework curator;
+
+    private static CuratorFramework createCurator(String connectString) {
+        Assert.notBlank(connectString, "Parameter \"connectString\" must not blank. ");
+        CuratorFramework curator = CuratorFrameworkFactory.builder()
+                .retryPolicy(new ExponentialBackoffRetry(1000, Integer.MAX_VALUE))
+                .sessionTimeoutMs(30000)
+                .connectionTimeoutMs(30000)
+                .connectString(connectString)
+                .build();
+        curator.start();
+        return curator;
+    }
+
+    public ZookeeperLockManager(String connectString) {
+
+        this(createCurator(connectString));
+    }
+
+    public ZookeeperLockManager(Map<String, InterProcessMutex> storage,
+                                String connectString) {
+
+        this(storage, createCurator(connectString));
+    }
+
+    public ZookeeperLockManager(CuratorFramework curator) {
+
+        this(new ConcurrentHashMap<String, InterProcessMutex>(), curator);
+    }
+
+    public ZookeeperLockManager(Map<String, InterProcessMutex> storage,
+                                CuratorFramework curator) {
+        Assert.notNull(storage, "Parameter \"storage\" must not null. ");
+        Assert.notNull(curator, "Parameter \"curator\" must not null. ");
+        this.storage = storage;
+        this.curator = curator;
+    }
+
+    @Override
+    public InterProcessMutex getLock(String lockName) {
+        Assert.notBlank(lockName, "Parameter \"lockName\" must not blank. ");
+        InterProcessMutex lock = storage.get(lockName);
+        if (lock != null) { return lock; }
+        synchronized (this) {
+            if ((lock = storage.get(lockName)) != null) {
+                return lock;
+            }
+            String path = PATH_PREFIX + SLASH + lockName;
+            storage.put(lockName, lock = new InterProcessMutex(curator, path));
+        }
+        return lock;
+    }
+
+    @Override
+    public Object removeLock(String lockName) {
+
+        return storage.remove(lockName);
+    }
+
+    @Override
+    public void lock(String lockName) {
+        try {
+            getLock(lockName).acquire();
+        }
+        catch (Exception e) {
+            throw ExceptionUtils.wrap(e);
+        }
+    }
+
+    @Override
+    public void unlock(String lockName) {
+        try {
+            getLock(lockName).release();
+        }
+        catch (Exception e) {
+            throw ExceptionUtils.wrap(e);
+        }
+    }
+
+    @Override
+    public void lockInterruptibly(String lockName) throws InterruptedException {
+
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean tryLock(String lockName) {
+
+        return tryLock(lockName, Constants.FIVE_HUNDRED, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public boolean tryLock(String lockName, long time, TimeUnit unit) {
+        try {
+            return getLock(lockName).acquire(time, unit);
+        }
+        catch (Exception e) {
+            throw ExceptionUtils.wrap(e);
+        }
+    }
+
+}
---
+++ src/test/java/artoria/lock/ZookeeperLockUtilsTest.java
@@ -0,0 +1,118 @@
+package artoria.lock;
+
+import artoria.util.ThreadUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.*;
+
+import static artoria.common.Constants.ZERO;
+
+@Ignore
+public class ZookeeperLockUtilsTest {
+    private static final Logger log = LoggerFactory.getLogger(ZookeeperLockUtilsTest.class);
+    private volatile Integer num = 100;
+    private final Integer threadNum = 50;
+    private ExecutorService threadPool;
+    private String managerName;
+
+    @Before
+    public void init() {
+        ThreadFactory threadFactory = Executors.defaultThreadFactory();
+        threadPool = new ThreadPoolExecutor(threadNum, threadNum, ZERO, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>(), threadFactory);
+        managerName = "zk";
+        LockUtils.registerManager(managerName, new ZookeeperLockManager("127.0.0.1:2181,127.0.0.2:2181,127.0.0.3:2181"));
+    }
+
+    @After
+    public void destroy() {
+
+        threadPool.shutdown();
+    }
+
+    private void bizCalc() {
+        num = num + 20;
+        ThreadUtils.sleepQuietly(50);
+        num = num - 40;
+        ThreadUtils.sleepQuietly(100);
+        num = num + 20;
+        num = num + 10;
+        ThreadUtils.sleepQuietly(150);
+        num = num - 20;
+        num = num + 10;
+        num = num - 1;
+    }
+
+    @Test
+    public void testLock() {
+        final String lockName = "testLock";
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                String threadName = Thread.currentThread().getName();
+                long millis = System.currentTimeMillis();
+                for (int j = 0; j < 1000000; j++) {
+                    if (num <= 0) { continue; }
+                    LockUtils.lock(managerName, lockName);
+                    try {
+                        log.info(">> {} lock", threadName);
+                        if (num > 0) {
+                            bizCalc();
+                            log.info("|| {} | {}", threadName, num);
+                        }
+                    }
+                    finally {
+                        log.info("<< {} unlock\n", threadName);
+                        LockUtils.unlock(managerName, lockName);
+                        ThreadUtils.sleepQuietly(100);
+                    }
+                }
+                log.info("{}: {}ms", threadName, System.currentTimeMillis() - millis);
+            }
+        };
+        for (int i = 0; i < threadNum; i++) {
+            threadPool.submit(runnable);
+        }
+        ThreadUtils.sleepQuietly(100000);
+    }
+
+    @Test
+    public void testTryLock() {
+        final String lockName = "testTryLock";
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                String threadName = Thread.currentThread().getName();
+                long millis = System.currentTimeMillis();
+                for (int j = 0; j < 1000000; j++) {
+                    if (num <= 0) { continue; }
+                    boolean tryLock = LockUtils.tryLock(managerName, lockName, 500, TimeUnit.MILLISECONDS);
+                    if (!tryLock) { continue; }
+                    try {
+                        log.info(">> {} tryLock {}", threadName, tryLock);
+                        if (num > 0) {
+                            bizCalc();
+                            log.info("|| {} | {}", threadName, num);
+                        }
+                    }
+                    finally {
+                        log.info("<< {} unlock\n", threadName);
+                        LockUtils.unlock(managerName, lockName);
+                        ThreadUtils.sleepQuietly(100);
+                    }
+                }
+                log.info("{}: {}ms", threadName, System.currentTimeMillis() - millis);
+            }
+        };
+        for (int i = 0; i < threadNum; i++) {
+            threadPool.submit(runnable);
+        }
+        ThreadUtils.sleepQuietly(100000);
+    }
+
+}
