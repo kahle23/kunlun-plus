c65ebc6b78cc0084afa3fce6cd6f3b65a5c9885a
Kahle <kahle@undefined>
2022-12-25 11:37:50 +0800

    Add automated asynchronous and operate related action handler

--- src/main/java/artoria/action/handler/AbstractAutoOperateActionHandler.java
+++ src/main/java/artoria/action/handler/AbstractAutoOperateActionHandler.java
@@ -3,27 +3,27 @@ package artoria.action.handler;
 import artoria.util.Assert;
 
 /**
- * The abstract automated classic action handler.
+ * The abstract automated operate action handler.
  * @author Kahle
  */
-public abstract class AbstractAutoClassicActionHandler
-        extends AbstractClassicActionHandler implements AutoActionHandler {
+public abstract class AbstractAutoOperateActionHandler
+        extends AbstractOperateActionHandler implements AutoActionHandler {
     private final HandlerConfigImpl handlerConfig;
     private final String actionName;
 
     @Deprecated
-    public AbstractAutoClassicActionHandler(String actionName, Class<?> registeredClass) {
+    public AbstractAutoOperateActionHandler(String actionName, Class<?> registeredClass) {
         this(actionName);
         Assert.notNull(registeredClass, "Parameter \"registeredClass\" must not null. ");
         getConfig().put("registeredClass", registeredClass.getName());
     }
 
-    public AbstractAutoClassicActionHandler(String actionName) {
+    public AbstractAutoOperateActionHandler(String actionName) {
 
         this(actionName, new HandlerConfigImpl());
     }
 
-    public AbstractAutoClassicActionHandler(String actionName, HandlerConfigImpl handlerConfig) {
+    public AbstractAutoOperateActionHandler(String actionName, HandlerConfigImpl handlerConfig) {
         Assert.notNull(handlerConfig, "Parameter \"handlerConfig\" must not null. ");
         Assert.notBlank(actionName, "Parameter \"actionName\" must not blank. ");
         this.handlerConfig = handlerConfig;
