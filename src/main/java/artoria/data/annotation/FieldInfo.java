e5b451f3c9778a6206779d6581f27a719d1f9452
Kahle <kahle@undefined>
2023-03-06 22:28:01 +0800

    Add java object field difference comparator

---
+++ src/main/java/artoria/data/annotation/FieldInfo.java
@@ -0,0 +1,34 @@
+package artoria.data.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * The field information.
+ * @author Kahle
+ */
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FieldInfo {
+
+    /**
+     * Get the field description.
+     * @return The field description
+     */
+    String description() default "";
+
+    /**
+     * Get the extra information for the field.
+     * @return The extra information
+     */
+    String extra() default "";
+
+    /**
+     * Get the field sort.
+     * @return The field sort
+     */
+    int    sort()  default 0;
+
+}
---
+++ src/main/java/artoria/data/comparison/support/difference/AbstractFieldComparator.java
@@ -0,0 +1,89 @@
+package artoria.data.comparison.support.difference;
+
+import artoria.core.Comparator;
+import artoria.data.annotation.FieldInfo;
+import artoria.exception.ExceptionUtils;
+import artoria.reflect.ReflectUtils;
+import artoria.util.ArrayUtils;
+import artoria.util.ObjectUtils;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static artoria.common.Constants.EMPTY_STRING;
+
+/**
+ * The abstract field comparator.
+ * @author Kahle
+ */
+public abstract class AbstractFieldComparator implements Comparator {
+
+    protected String describe(Field field) {
+        FieldInfo annotation = field.getAnnotation(FieldInfo.class);
+        return annotation != null ? annotation.description() : EMPTY_STRING;
+    }
+
+    protected List<FieldEntity> convert(Object object) {
+        try {
+            Field[] fields = ReflectUtils.getDeclaredFields(object.getClass());
+            if (ArrayUtils.isEmpty(fields)) { return Collections.emptyList(); }
+            List<FieldEntity> result = new ArrayList<FieldEntity>();
+            for (Field field : fields) {
+                ReflectUtils.makeAccessible(field);
+                FieldEntity entity = new FieldEntity();
+                entity.setName(field.getName());
+                entity.setValue(field.get(object));
+                entity.setDescription(describe(field));
+                result.add(entity);
+            }
+            return result;
+        }
+        catch (Exception e) {
+            throw ExceptionUtils.wrap(e);
+        }
+    }
+
+    protected boolean equals(Object leftValue, Object rightValue) {
+
+        return ObjectUtils.equals(leftValue, rightValue);
+    }
+
+    protected static class FieldEntity {
+        private String description;
+        private String name;
+        private Object value;
+
+        public String getName() {
+
+            return name;
+        }
+
+        public void setName(String name) {
+
+            this.name = name;
+        }
+
+        public String getDescription() {
+
+            return description;
+        }
+
+        public void setDescription(String description) {
+
+            this.description = description;
+        }
+
+        public Object getValue() {
+
+            return value;
+        }
+
+        public void setValue(Object value) {
+
+            this.value = value;
+        }
+    }
+
+}
