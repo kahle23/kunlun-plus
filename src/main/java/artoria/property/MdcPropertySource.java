7fc31964fc640219bab3048f9703c93dc552325a
Kahle <kahle@undefined>
2023-03-28 00:17:20 +0800

    Add property source related code

---
+++ src/main/java/artoria/property/MdcPropertySource.java
@@ -0,0 +1,39 @@
+package artoria.property;
+
+import artoria.util.ObjectUtils;
+import org.slf4j.MDC;
+
+import java.util.Map;
+
+public class MdcPropertySource extends AbstractPropertySource {
+
+    public MdcPropertySource(String name) {
+
+        super(name);
+    }
+
+    @Override
+    public Map<String, Object> getProperties() {
+
+        return ObjectUtils.cast(MDC.getCopyOfContextMap());
+    }
+
+    @Override
+    public Object setProperty(String name, Object value) {
+        MDC.put(name, String.valueOf(value));
+        return null;
+    }
+
+    @Override
+    public Object getProperty(String name) {
+
+        return MDC.get(name);
+    }
+
+    @Override
+    public Object removeProperty(String name) {
+        MDC.remove(name);
+        return null;
+    }
+
+}
