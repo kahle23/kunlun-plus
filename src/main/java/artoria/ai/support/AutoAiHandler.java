9f9a289571205c769911cdcc8e53b5b4aae086f1
Kahle <kahle@undefined>
2023-08-06 11:43:55 +0800

    Add automated ai handler related code

---
+++ src/main/java/artoria/ai/support/AutoAiHandler.java
@@ -0,0 +1,13 @@
+package artoria.ai.support;
+
+import artoria.core.ArtificialIntelligence;
+
+public interface AutoAiHandler extends ArtificialIntelligence {
+
+    /**
+     * Get the ai handler name.
+     * @return The ai handler name
+     */
+    String getName();
+
+}
