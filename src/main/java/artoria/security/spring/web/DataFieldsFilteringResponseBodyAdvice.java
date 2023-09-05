5fdf3e001fd7dd645a441b7ff10d7bcc2cb18b70
Kahle <kahle@undefined>
2023-09-05 21:36:49 +0800

    Add "ResourceAccessFilter", "ResourceAccessSpringInterceptor" and "DataFieldsFilteringResponseBodyAdvice"

---
+++ src/main/java/artoria/security/spring/web/DataFieldsFilteringResponseBodyAdvice.java
@@ -0,0 +1,43 @@
+package artoria.security.spring.web;
+
+import artoria.core.handler.DataFieldsFilteringHandler;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import javax.annotation.Resource;
+
+/**
+ * The response body advice for the data fields filtering.
+ * @author Kahle
+ */
+@RestControllerAdvice
+public class DataFieldsFilteringResponseBodyAdvice implements ResponseBodyAdvice<Object> {
+    private static final String FILTERING_TYPE = "response-body-advice-data-permission";
+
+    @Resource
+    private DataFieldsFilteringHandler filteringHandler;
+
+    @Override
+    public boolean supports(@NonNull MethodParameter returnType,
+                            @NonNull Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body,
+                                  @NonNull MethodParameter returnType,
+                                  @NonNull MediaType selectedContentType,
+                                  @NonNull Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  @NonNull ServerHttpRequest request,
+                                  @NonNull ServerHttpResponse response) {
+        return filteringHandler.filter(FILTERING_TYPE, body
+                , returnType, selectedContentType, selectedConverterType, request, response);
+    }
+
+}
---
+++ src/main/java/artoria/security/spring/web/ResourceAccessSpringInterceptor.java
@@ -0,0 +1,42 @@
+package artoria.security.spring.web;
+
+import artoria.core.handler.ResourceAccessPreHandler;
+import artoria.util.Assert;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Support for resource access pre handler based on spring interceptor.
+ * @author Kahle
+ */
+public class ResourceAccessSpringInterceptor extends HandlerInterceptorAdapter {
+    private static final String ACCESS_TYPE = "spring-servlet-interceptor";
+    private static final String DEF_TOKEN_NAME = "authorization";
+    private final ResourceAccessPreHandler accessPreHandler;
+    private final String tokenName;
+
+    public ResourceAccessSpringInterceptor(ResourceAccessPreHandler accessPreHandler, String tokenName) {
+        Assert.notNull(accessPreHandler, "Parameter \"accessPreHandler\" must not null. ");
+        Assert.notBlank(tokenName, "Parameter \"tokenName\" must not blank. ");
+        this.accessPreHandler = accessPreHandler;
+        this.tokenName = tokenName;
+    }
+
+    public ResourceAccessSpringInterceptor(ResourceAccessPreHandler accessPreHandler) {
+
+        this(accessPreHandler, DEF_TOKEN_NAME);
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             @NonNull HttpServletResponse response,
+                             @NonNull Object handler) throws Exception {
+        String token = request.getHeader(tokenName);
+        String path = request.getServletPath();
+        return (Boolean) accessPreHandler.handle(ACCESS_TYPE, path, token, request, response);
+    }
+
+}
